c5f361a495aa62ac38e0320a23a33deda5adf588
Pranav <Pranav@DESKTOP-S1A74RE>
2022-04-07 17:38:34 -0700

    new file

---
+++ cyb595test/src/main/java/cyb595test/junit_test2.java
@@ -0,0 +1,22 @@
+package cyb595test;
+
+import org.junit.Assert;
+import org.junit.*;
+
+public class junit_test2 {
+
+	@Test
+    public void testCheckSignShouldReturnPositive() {
+        MySimpleMath sm = new MySimpleMath();
+        Assert.assertEquals("positive", sm.checkSign(5));
+        Assert.assertEquals("positive", sm.checkSign(0));
+    }
+    
+    @Test
+    public void testCheckSignShouldReturnNegative() {
+        MySimpleMath sm = new MySimpleMath();
+        Assert.assertEquals("negative", sm.checkSign(-5));
+    }
+
+}
+
