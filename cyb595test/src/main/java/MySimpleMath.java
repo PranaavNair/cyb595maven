810cc596469371cb6aef1e2c76158c07ed478964
Pranav <Pranav@DESKTOP-S1A74RE.home>
2022-04-12 17:24:18 -0700

    new files

---
+++ cyb595test/src/main/java/MySimpleMath.java
@@ -0,0 +1,12 @@
+
+
+public class MySimpleMath {
+	
+	public String checkSign(int number) {
+        if(number >= 0 ) {
+            return "positive";
+        } else {
+            return "negative";
+        }
+    }
+}
